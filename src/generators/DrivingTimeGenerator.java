76a23b69753438ebc4c36c8f1c714214efbbe774
Jessica Bran <jessica.m.bran@gmail.com>
2019-10-17 13:33:23 +0200

    Added functions for validation in the generators

--- src/generators/DrivingTimeGenerator.java
+++ src/generators/DrivingTimeGenerator.java
@@ -15,4 +15,24 @@ public  class DrivingTimeGenerator {
         else
             return 10;
     }
+
+    public static double generateDrivingTimeValidation(Stop currentStop, Stop nextStop, int valNumber){
+
+        int[] avgDrivingTimes = {134,243,59,101,60,86,78,113,110,0,110,78,82,60,100,59,243,135};
+
+        Random r = new Random();
+        double random = r.nextFloat();
+        if (random < 0.4){
+            return 0.8*avgDrivingTimes[currentStop.getStopNumber()];
+        } else if (random >= 0.4 && random < 0.7){
+            return avgDrivingTimes[currentStop.getStopNumber()];
+        } else if (random >= 0.7 && random < 0.9){
+            return avgDrivingTimes[currentStop.getStopNumber()] * 1.2;
+        } else {
+            return avgDrivingTimes[currentStop.getStopNumber()] * 1.4;
+        }
+
+    }
+
+
 }
--- src/generators/PassengersOutGenerator.java
+++ src/generators/PassengersOutGenerator.java
@@ -12,6 +12,19 @@ import java.util.Random;
 
 public class PassengersOutGenerator {
 
+    public static int getNumPassengersValidation(Tram tram, Stop stop, LocalTime time, int valNumber) throws IOException {
+                double prob = getStopMeanValidation(stop, time, valNumber);
+        Random r = new Random();
+        int counter = 0;
+        for (int i = 0; i < tram.getPassengersNumber(); i++){
+            double random = r.nextFloat();
+            if (random < prob){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
     public static int getNumPassengers(Tram tram, Stop stop, LocalTime time) throws IOException {
         double prob = getStopMean(stop, time);
         Random r = new Random();
@@ -32,6 +45,29 @@ public class PassengersOutGenerator {
             means = getMeans("src/files/probOutRouteB.txt");
         } else {
             means = getMeans("src/files/probOutRouteA.txt");
+        }
+
+
+        if (stop.getStopNumber() == 1 || stop.getStopNumber() == 18){
+            return means[getTimePeriod(time)][9];
+        } else if (stop.getStopNumber() > 1 && stop.getStopNumber() <= 8){
+            return means[getTimePeriod(time)][stop.getStopNumber()-1];
+        } else if (stop.getStopNumber() == 9 || stop.getStopNumber() == 10){
+            return means[getTimePeriod(time)][9];
+        } else {
+            return means[getTimePeriod(time)][stop.getStopNumber()%10];
+        }
+
+    }
+
+    private static double getStopMeanValidation(Stop stop, LocalTime time, int valNumber) throws IOException {
+        double[][] means;
+        String fileName = "validation" + valNumber + "Out";
+
+        if (stop.getStopNumber() >= 1 && stop.getStopNumber() < 9 || stop.getStopNumber() == 18){
+            means = getMeans("src/files/validation/" + fileName + "B.txt");
+        } else {
+            means = getMeans("src/files/validation/" + fileName + "A.txt");
         }
 
 
