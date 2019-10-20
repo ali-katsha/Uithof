4d89c98e9b2881dfd1a33528f35261dfed896aca
Jessica Bran <jessica.m.bran@gmail.com>
2019-10-20 20:35:07 +0200

    Created dwell time generator

---
+++ src/generators/DwellTimeGenerator.java
@@ -0,0 +1,19 @@
+package generators;
+
+import org.apache.commons.math3.distribution.GammaDistribution;
+
+public class DwellTimeGenerator {
+
+    public double generateDwellTime(int passIn, int passOut){
+        double d = 12.5 + 0.22*passIn + 0.13*passOut;
+        double dwellTime = 0.0;
+
+        GammaDistribution dwellDist = new GammaDistribution(2,d/2);
+
+        do {
+            dwellTime = dwellDist.sample();
+        } while (dwellTime < (d*0.8));
+
+        return dwellTime;
+    }
+}
